79a6109fd4e96e59cde85ba1d036a6c046cbd9cf
st-devsecops <144426060+st-devsecops@users.noreply.github.com>
2025-11-18 16:11:35 +0530

    Create data.py

---
+++ Basic/data.py
@@ -0,0 +1,26 @@
+data1 = [
+    {
+        'name': 'Swetha',
+        'follower_count': 346
+    },
+    {
+        'name': 'Sri',
+        'follower_count': 215
+    },
+    {
+        'name': 'Chako',
+        'follower_count': 183
+    },
+    {
+        'name': 'Blacky',
+        'follower_count': 348
+    },
+    {
+        'name': 'Whitey',
+        'follower_count': 219
+    },
+    {
+        'name': 'janny',
+        'follower_count': 185
+    }
+]
